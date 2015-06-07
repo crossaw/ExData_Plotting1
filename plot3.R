d0b098bde5b14f68b6eebf9ef11131a5cbb15d4a
crossaw <ammmj@ez2.net>
2015-06-07 09:46:13 -0700

    Finished plot3

--- plot3.R
+++ plot3.R
@@ -1,48 +1,53 @@
-# plot2.R
-# This script creates a plot of ... 
-
-#from the Global_active_power column of the
-# file idetified below as fn.  The histogram is saved to the file identified
-# as outFile.  The input data is left in the workspace as a dataframe, hpc.
+# plot3.R
+# This script creates a line plot of the three Sub Metering variables for 1 Jul
+# and 2 Jul, 2007 from the data in the file identified by fn.  The plot is saved
+# to the file identified as outfile.  The input data is left in the workspace as
+# a dataframe, hpc.
 
 library(dplyr)
 
+### FILENAMES AND CONSTANTS
 fn = "household_power_consumption.txt"
-outFile = "plot1.png"
+outFile = "plot3.png"
+lc = c("black", "red", "blue")   # line colors
 
+### LOAD THE DATA
 cc <- c( rep("character",2), rep("numeric", 7) )
-if(F){
 hpc <- filter( read.table(fn, header=T, na.strings="?", colClasses=cc, sep=";"),
       Date=="1/2/2007" | Date=="2/2/2007" )
 hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
-hpc$Time <- strptime(hpc$Time, "%H:%M:%S")
-}
-#png(outFile)
-day <- weekdays(hpc$Date, T)
+
+### CREATE THE PLOT
+png(outFile)
+
+# without tick marks on the x axis
 plot(hpc[,7], type="l", ylab="Energy sub-metering", xaxt="n")
-lines(x=1:nrow(hpc), y=hpc[,8], col="red")
-lines(x=1:nrow(hpc), y=hpc[,9], col="blue")
+lines(x=1:nrow(hpc), y=hpc[,8], col=lc[2])
+lines(x=1:nrow(hpc), y=hpc[,9], col=lc[3])
 
-xlab <- hpc[1,1]
-xind <- 1
+# prepare x axis tick marks
+xlab <- hpc[1,1]           # Initialize x labels.
+xloc <- 1                  # Initialize x tick locations.
 
-for (i in 1:nrow(hpc)) {
+for (i in 1:nrow(hpc)) {   # Populate x tick locations and labels
    if (hpc[i,1] != xlab[length(xlab)]) {
 
       xlab <- c(xlab, hpc[i,1])
-      xind <- c(xind, i)
+      xloc <- c(xloc, i)
    }
 }
-if (xind[length(xind)] != nrow(hpc)) {
+# Label the last point for the next day if it is not already a new day.
+if (xloc[length(xloc)] != nrow(hpc)) {
    xlab <- c( xlab, hpc[nrow(hpc),1]+1 )
-   xind <- c( xind, nrow(hpc) )
+   xloc <- c( xloc, nrow(hpc) )
 }
-
 xlab <- weekdays(xlab, T)
-#lines(x=1:nrow(hpc), y=hpc[,8])
+axis(1, xloc, xlab)
 
-axis(1, xind, xlab)
+# Add a legend.
+legend( "topright", names(hpc[7:9]), col=lc, lty=1 )
 
-#dev.off()
-
-#rm (fn, outFile, cc)
+### CLEANUP
+dev.off()
+# Leave just hpc.
+rm (fn, outFile, cc, xlab, xloc, i, lc)
