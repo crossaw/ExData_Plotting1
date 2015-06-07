522133baf357ed8ad66423bea9cdbd56885afcff
crossaw <ammmj@ez2.net>
2015-06-07 08:48:29 -0700

    Commenting and cleanup on plot1.R

--- plot1.R
+++ plot1.R
@@ -1,19 +1,21 @@
 # plot1.R
-# This script creates a histogram from the Global_active_power column of the
-# file idetified below as fn.  The histogram is saved to the file identified
-# as outFile.  The input data is left in the workspace as a dataframe, hpc.
+# This script creates a histogram of Global Active Power for 1 Jul and 2 Jul
+# from the data in the file idetified by fn.  The histogram is saved to the file
+# identified as outFile.  The input data is left in the workspace as
+# a dataframe, hpc.
 
 library(dplyr)
 
+### FILENAMES
 fn = "household_power_consumption.txt"
 outFile = "plot1.png"
 
+### LOAD THE DATA
 cc <- c( rep("character",2), rep("numeric", 7) )
 hpc <- filter( read.table(fn, header=T, na.strings="?", colClasses=cc, sep=";"),
-      Date=="1/2/2007" | Date=="2/2/2007" )
-hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
-hpc$Time <- strptime(hpc$Time, "%H:%M:%S")
+         Date=="1/2/2007" | Date=="2/2/2007" )
 
+### CREATE THE PLOT
 png(outFile)
 
 brks = 0 : ceiling( 2 * max(hpc$Global_active_power) ) / 2
@@ -22,4 +24,6 @@ hist(hpc$Global_active_power, breaks = brks, freq=T, col="red",
 
 dev.off()
 
+### CLEANUP
+# Leave just hpc.
 rm (fn, outFile, cc, brks)
